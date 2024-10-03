a383e7de1ca872f398a49923a76aff334e983b78
HopelessJu <s.tsikhmanovich@gmail.com>
2024-10-03 14:43:53 +0200

    feat: add cli solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const propNames = args.filter((prop) => prop.startsWith("--"));
+  const propValues = args.filter((prop) => !prop.startsWith("--"));
+
+  let result = [];
+  for (let i = 0; i < propNames.length; i++) {
+    result = [...result, `${propNames[i]} is ${propValues[i]}`];
+  }
+  console.log(result.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envsToParse = Object.keys(process.env).filter((key) =>
+    key.startsWith("RSS_")
+  );
+  envsToParse.map((envVar) => {
+    const envName = `${envVar}=${process.env[envVar]}`;
+    console.log(envName);
+  });
 };
 
 parseEnv();
