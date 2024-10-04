03ed6da55a5b6ecc4753240ffd0703bb8bac345f
HopelessJu <s.tsikhmanovich@gmail.com>
2024-10-04 13:13:45 +0200

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { stdout, exit } from "process";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files/fileToCalculateHashFor.txt");
+  const hash = createHash("sha256");
+
+  const stream = createReadStream(filePath);
+
+  stream.on("error", (error) => {
+    console.error(error.message);
+    exit(1);
+  });
+
+  hash.on("error", (error) => {
+    console.error(`Hashing error: ${error.message}`);
+    exit(1);
+  });
+
+  stream.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
