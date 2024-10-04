17c24af0b4aae44b833b6495ddf90f0265d428f0
HopelessJu <s.tsikhmanovich@gmail.com>
2024-10-04 13:55:04 +0200

    feat: add stream solutions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from "fs";
+import { stdout } from "process";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files/fileToRead.txt");
+  const readStream = createReadStream(filePath);
+  readStream.on("error", (error) => {
+    console.error(error.message);
+  });
+
+  readStream.pipe(stdout);
 };
 
 await read();
