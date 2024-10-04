4418e06fb2b4b97cf89b7b33db3ef1c14d5068a2
HopelessJu <s.tsikhmanovich@gmail.com>
2024-10-04 16:45:20 +0200

    feat: add solution for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import { fork } from "child_process";
+import { stdin, stdout } from "process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = "./files/script.js";
+  const pathUrl = new URL(scriptPath, import.meta.url);
+  const childProcess = fork(pathUrl, args, {
+    stdio: ["pipe", "pipe", "inherit", "ipc"],
+  });
+  stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(stdout);
+
+  childProcess.on("message", (msg) => {
+    console.log(`Message from child: ${msg}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/*["arg1", "arg2", "arg3"]*/);
